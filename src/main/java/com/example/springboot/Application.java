67756c65620e1d86ff8c5d313973701e110c30a1
Gan Ze Yu <gan.zeyu@flex-solver.com.sg>
2022-11-03 22:15:23 +0800

    init

---
+++ src/main/java/com/example/springboot/Application.java
@@ -0,0 +1,36 @@
+package com.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.saml2.provider.service.authentication.Saml2AuthenticatedPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@SpringBootApplication
+@Controller
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/")
+	public String index() {
+		return "home";
+	}
+
+	/***
+	 * API Secured by Spring Security SAMl2
+	 * @param principal - retrieve the principal (user) logged in with annotation @AuthenticationPrincipal
+	 * @param model - Springboot ModelMap
+	 * @return
+	 */
+	@RequestMapping("/secured/hello")
+	public String hello(@AuthenticationPrincipal Saml2AuthenticatedPrincipal principal, Model model) {
+		model.addAttribute("name", principal.getName());
+		return "hello";
+	}
+
+}
