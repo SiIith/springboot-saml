005587288eb6e8ea8d265a2845e38af2d68b8b3a
Gan Ze Yu <gan.zeyu@flex-solver.com.sg>
2022-11-13 21:17:22 +0800

    google auth component

---
+++ src/main/java/com/example/springboot/Auth/ValidateCodeDto.java
@@ -0,0 +1,13 @@
+package com.example.springboot.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidateCodeDto {
+    private Integer code;
+    private String username;
+}
---
+++ src/main/java/com/example/springboot/Auth/Validation.java
@@ -0,0 +1,12 @@
+package com.example.springboot.Auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Validation {
+    private boolean valid;
+}
